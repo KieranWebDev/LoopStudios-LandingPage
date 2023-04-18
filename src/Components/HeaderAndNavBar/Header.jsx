6fd386b2e1b0e03ac8e9c5b719d13ab2ea58d221
Kieran <kieran.webdevelopment@gmail.com>
2023-04-18 16:40:04 +0100

    responsive navbar complete

--- src/Components/HeaderAndNavBar/Header.jsx
+++ src/Components/HeaderAndNavBar/Header.jsx
@@ -1,11 +1,14 @@
 import NavBar from './NavBar.jsx';
-
+// styles
 import './Header.css';
+
 export default function Header() {
   return (
     <header>
-      <NavBar />
-      <h1>Header</h1>
+      <div className="header-container">
+        <NavBar />
+        <h1>Header</h1>
+      </div>
     </header>
   );
 }
--- src/Components/HeaderAndNavBar/NavBar.jsx
+++ src/Components/HeaderAndNavBar/NavBar.jsx
@@ -1,4 +1,6 @@
+// styles
 import './NavBar.css';
+// images
 import Logo from '../../assets/images/logo.svg';
 import Hamburger from '../../assets/images/icon-hamburger.svg';
 
@@ -6,9 +8,26 @@ export default function NavBar() {
   return (
     <nav>
       <img className="logo" src={Logo} alt="Loop Studios" />
-      <button>
-        <img className="hamburger" src={Hamburger} alt="menu button" />
+      <button className="hamburger">
+        <img src={Hamburger} alt="menu button" />
       </button>
+      <ul className="menu">
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Careers</a>
+        </li>
+        <li>
+          <a href="#">Events</a>
+        </li>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Support</a>
+        </li>
+      </ul>
     </nav>
   );
 }
