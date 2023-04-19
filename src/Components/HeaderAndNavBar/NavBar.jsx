4785f461095ff880bfa0840d7b2285a853bb1961
Kieran <kieran.webdevelopment@gmail.com>
2023-04-19 14:50:03 +0100

    mobile menu skeleton added

--- src/Components/HeaderAndNavBar/NavBar.jsx
+++ src/Components/HeaderAndNavBar/NavBar.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 // styles
 import './NavBar.css';
 // images
 import Logo from '../../assets/images/logo.svg';
 import Hamburger from '../../assets/images/icon-hamburger.svg';
+import Close from '../../assets/images/icon-close.svg';
 
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <nav>
-      <img className="logo" src={Logo} alt="Loop Studios" />
-      <button className="hamburger">
-        <img src={Hamburger} alt="menu button" />
-      </button>
-      <ul className="menu">
+    <nav className={isOpen ? 'mobile-menu' : ''}>
+      <div className="logo-and-hamburger">
+        <img className="logo" src={Logo} alt="Loop Studios" />
+        <button onClick={toggleMenu} className="hamburger">
+          <img src={isOpen ? Close : Hamburger} alt="menu button" />
+        </button>
+      </div>
+      <ul className={isOpen ? 'mobile-links' : 'desktop-links'}>
         <li>
           <a href="#">About</a>
         </li>
