776dd4c3180cc4c1f8972ac9a126e52d7a18c7da
Kieran <kieran.webdevelopment@gmail.com>
2023-04-21 12:08:49 +0100

    updated data file

--- src/Components/OurCreations/OurCreations.jsx
+++ src/Components/OurCreations/OurCreations.jsx
@@ -1,7 +1,8 @@
 import './OurCreations.css';
+import { creationSectionData } from '../../data.jsx';
 
-import DeepEarthMobile from '../../assets/images/mobile/image-deep-earth.jpg';
-import DeepEarthDesktop from '../../assets/images/desktop/image-deep-earth.jpg';
+// import DeepEarthMobile from '../../assets/images/mobile/image-deep-earth.jpg';
+// import DeepEarthDesktop from '../../assets/images/desktop/image-deep-earth.jpg';
 
 export default function OurCreations() {
   return (
@@ -12,14 +13,14 @@ export default function OurCreations() {
         <div className="our-creations-card">
           <img
             className="our-creation-pic"
-            src={DeepEarthMobile}
-            srcSet={`${DeepEarthMobile} 654w, ${DeepEarthDesktop}`}
-            sizes="(max-width: 700px) 651px, 100vw"
+            src={creationSectionData[0].mobileImg}
+            srcSet={`${creationSectionData[0].mobileImg} 654w, ${creationSectionData[0].desktopImg}`}
+            sizes="(max-width: 800px) 651px, 100vw"
             alt="person using a VR headset"
           />
           <h3>Deep earth</h3>
         </div>
-        <div className="our-creations-card">
+        {/* <div className="our-creations-card">
           <img
             className="our-creation-pic"
             src={DeepEarthMobile}
@@ -48,7 +49,7 @@ export default function OurCreations() {
             alt="person using a VR headset"
           />
           <h3>Deep earth</h3>
-        </div>
+        </div> */}
       </div>
     </section>
   );
--- src/data.jsx
+++ src/data.jsx
@@ -1,42 +1,65 @@
-const creationSectionData = [
+import DeepEarthMobile from './assets/images/mobile/image-deep-earth.jpg';
+import DeepEarthDesktop from './assets/images/desktop/image-deep-earth.jpg';
+import NightArcadeMobile from './assets/images/mobile/image-night-arcade.jpg';
+import NightArcadeDesktop from './assets/images/desktop/image-night-arcade.jpg';
+import SoccerTeamMobile from './assets/images/mobile/image-soccer-team.jpg';
+import SoccerTeamDesktop from './assets/images/desktop/image-soccer-team.jpg';
+import TheGridMobile from './assets/images/mobile/image-grid.jpg';
+import TheGridDesktop from './assets/images/desktop/image-grid.jpg';
+import FromAboveMobile from './assets/images/mobile/image-from-above.jpg';
+import FromAboveDesktop from './assets/images/desktop/image-from-above.jpg';
+import PocketBorealisMobile from './assets/images/mobile/image-pocket-borealis.jpg';
+import PocketBorealisDesktop from './assets/images/desktop/image-pocket-borealis.jpg';
+import CuriosityMobile from './assets/images/mobile/image-curiosity.jpg';
+import CuriosityDesktop from './assets/images/desktop/image-curiosity.jpg';
+import FisheyeMobile from './assets/images/mobile/image-fisheye.jpg';
+
+export const creationSectionData = [
   {
     title: 'Deep earth',
-    mobileImg: '../../assets/images/mobile/image-deep-earth.jpg',
-    desktopImg: '../../assets/images/desktop/image-deep-earth.jpg',
+    mobileImg: DeepEarthMobile,
+    desktopImg: DeepEarthDesktop,
+    alt: 'picture of Earth',
   },
   {
     title: 'Night arcade',
-    mobileImg: '../../assets/images/mobile/image-night-arcade.jpg',
-    desktopImg: '../../assets/images/desktop/image-night-arcade.jpg',
+    mobileImg: NightArcadeMobile,
+    desktopImg: NightArcadeDesktop,
+    alt: 'picture of a city at night',
   },
   {
     title: 'Soccer team VR',
-    mobileImg: '../../assets/images/mobile/image-soccer-team.jpg',
-    desktopImg: '../../assets/images/desktop/image-soccer-team.jpg',
+    mobileImg: SoccerTeamMobile,
+    desktopImg: SoccerTeamDesktop,
+    alt: 'picture of a soccer team',
   },
   {
     title: 'The grid',
-    mobileImg: '../../assets/images/mobile/image-grid.jpg',
-    desktopImg: '../../assets/images/desktop/image-grid.jpg',
+    mobileImg: TheGridMobile,
+    desktopImg: TheGridDesktop,
+    alt: 'picture of a grid',
   },
   {
     title: 'From up above VR',
-    mobileImg: '../../assets/images/mobile/image-from-above.jpg',
-    desktopImg: '../../assets/images/desktop/image-from-above.jpg',
+    mobileImg: FromAboveMobile,
+    desktopImg: FromAboveDesktop,
+    alt: 'picture of a city from above',
   },
   {
     title: 'Pocket borealis',
-    mobileImg: '../../assets/images/mobile/image-pocket-borealis.jpg',
-    desktopImg: '../../assets/images/desktop/image-pocket-borealis.jpg',
+    mobileImg: PocketBorealisMobile,
+    desktopImg: PocketBorealisDesktop,
+    alt: 'picture of northern lights',
   },
   {
     title: 'The curiosity',
-    mobileImg: '../../assets/images/mobile/image-curiosity.jpg',
-    desktopImg: '../../assets/images/desktop/image-curiosity.jpg',
+    mobileImg: CuriosityMobile,
+    desktopImg: CuriosityDesktop,
+    alt: 'picture of a rover on Mars',
   },
   {
     title: 'Make it fisheye',
-    mobileImg: '../../assets/images/mobile/image-fisheye.jpg',
-    desktopImg: '../../assets/images/desktop/image-fisheye.jpg',
+    mobileImg: FisheyeMobile,
+    alt: 'picture of a fisheye lens',
   },
 ];
