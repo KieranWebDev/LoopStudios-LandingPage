a956b0a3a07bfdb1748b8fec5eeef4153b663fe5
Kieran <kieran.webdevelopment@gmail.com>
2023-04-21 10:34:44 +0100

    OurCreations Component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Components/HeaderAndNavBar/Header';
 import WebsiteIntro from './Components/WebsiteIntro/WebsiteIntro';
 // styles
 import './App.css';
+import OurCreations from './Components/OurCreations/OurCreations';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <div className="main-content-container">
         <WebsiteIntro />
+        <OurCreations />
       </div>
     </div>
   );
---
+++ src/Components/OurCreations/OurCreations.jsx
@@ -0,0 +1,9 @@
+import './OurCreations.css';
+
+export default function OurCreations() {
+  return (
+    <section className="our-creations-section">
+      <h2>Our Creations</h2>
+    </section>
+  );
+}
