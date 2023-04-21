ae96ae68f876370f6adb63871dd215fce825c6e3
Kieran <kieran.webdevelopment@gmail.com>
2023-04-21 11:52:18 +0100

    added data to data file

---
+++ src/data.jsx
@@ -0,0 +1,42 @@
+const creationSectionData = [
+  {
+    title: 'Deep earth',
+    mobileImg: '../../assets/images/mobile/image-deep-earth.jpg',
+    desktopImg: '../../assets/images/desktop/image-deep-earth.jpg',
+  },
+  {
+    title: 'Night arcade',
+    mobileImg: '../../assets/images/mobile/image-night-arcade.jpg',
+    desktopImg: '../../assets/images/desktop/image-night-arcade.jpg',
+  },
+  {
+    title: 'Soccer team VR',
+    mobileImg: '../../assets/images/mobile/image-soccer-team.jpg',
+    desktopImg: '../../assets/images/desktop/image-soccer-team.jpg',
+  },
+  {
+    title: 'The grid',
+    mobileImg: '../../assets/images/mobile/image-grid.jpg',
+    desktopImg: '../../assets/images/desktop/image-grid.jpg',
+  },
+  {
+    title: 'From up above VR',
+    mobileImg: '../../assets/images/mobile/image-from-above.jpg',
+    desktopImg: '../../assets/images/desktop/image-from-above.jpg',
+  },
+  {
+    title: 'Pocket borealis',
+    mobileImg: '../../assets/images/mobile/image-pocket-borealis.jpg',
+    desktopImg: '../../assets/images/desktop/image-pocket-borealis.jpg',
+  },
+  {
+    title: 'The curiosity',
+    mobileImg: '../../assets/images/mobile/image-curiosity.jpg',
+    desktopImg: '../../assets/images/desktop/image-curiosity.jpg',
+  },
+  {
+    title: 'Make it fisheye',
+    mobileImg: '../../assets/images/mobile/image-fisheye.jpg',
+    desktopImg: '../../assets/images/desktop/image-fisheye.jpg',
+  },
+];
